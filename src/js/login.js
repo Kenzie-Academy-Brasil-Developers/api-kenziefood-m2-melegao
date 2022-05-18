1f6fcb2cb8f27abf531bbfbc960b6b04b3be6461
Débora Gomes Corrêa <99352568+DebCorrea@users.noreply.github.com>
2022-05-18 20:29:18 -0300

    fix: ponto e vírgula adicionados

--- src/js/login.js
+++ src/js/login.js
@@ -1,8 +1,7 @@
-import { Usuario } from "../controller/Usuario.js"
+import { Usuario } from "../controller/Usuario.js";
 
 const form = document.getElementById('body__form--login');
 form.addEventListener('submit', receberDadosLogin);
-
 
 async function receberDadosLogin(e) {
 
@@ -13,18 +12,20 @@ async function receberDadosLogin(e) {
     const elements = [...form];
 
     for (let i = 0; i < elements.length; i++) {
-    let item = elements[i];
+        let item = elements[i];
 
-    if (item.name !== "") {
-        data[item.name] = item.value;
+        if (item.name !== "") {
+            data[item.name] = item.value;
         }
     }
 
     const retorno = await Usuario.logarUsuario(data);
 
-    if (retorno.status == 200){
+    if (retorno.status == 200) {
+        
         localStorage.setItem('token', retorno.token);
         location.replace('/src/pages/dashboard.html');
+        
     }
 
 }
