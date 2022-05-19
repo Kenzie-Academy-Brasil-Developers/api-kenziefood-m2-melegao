43b48e3c4e1a5368a3d1b0010787eff6867d04a8
melegao <melegao@gmail.com>
2022-05-18 23:52:30 -0300

    feat: Desenvoldia a busca dinâmica

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,11 @@
 import { Produtos } from "../controller/Produtos.js";
 
-criarCards ('Todos');
+criarCards ();
 filtroCategoria();
 abrirCadastroLogin();
+pesquisaDinamica();
 
-async function criarCards (categoria) {
+async function criarCards (input) {
 
     const produtos = await Produtos.listarTodosProdutos();
 
@@ -13,31 +14,7 @@ async function criarCards (categoria) {
 
     produtos.forEach((elem) => {
 
-        if (categoria == elem.categoria) {
-
-            const cardProduto = document.createElement('article');
-            cardProduto.setAttribute('class', 'section__article--card');
-            cardProduto.setAttribute('id', elem.id);
-            
-            cardProduto.innerHTML = `
-            <img class="article__img--produto" src=${elem.imagem} alt="">
-            <h3 class="article__h3--tituloProduto">${elem.nome}</h3>
-            <div>
-                <p class="article__p--descricaoProduto">${elem.descricao}.</p>
-            </div>
-            <div class="article__div--categorias">
-                <span class="div__span--categoria">${elem.categoria}</span>
-            </div>
-            <div class="article__div--rodape">
-                <h3 class="div__h3--preco">R$ ${elem.preco.toFixed(2).replace('.',',')}</h3>
-                <button class="div__button--addCarrinho"> <img src="/src/imagens/botao-carrinho.png" alt="">
-                </button>
-            </div>
-            `;
-            
-            divCard.appendChild(cardProduto);
-            
-        } else if (categoria == 'Todos') {
+        if (input == 'todos' || input == '' || input == null) {
             
             const cardProduto = document.createElement('article');
             cardProduto.setAttribute('class', 'section__article--card');
@@ -61,6 +38,30 @@ async function criarCards (categoria) {
             
             divCard.appendChild(cardProduto);
 
+        } else if (elem.nome.toLowerCase().includes(input) || elem.categoria.toLowerCase().includes(input)) {
+            
+            const cardProduto = document.createElement('article');
+            cardProduto.setAttribute('class', 'section__article--card');
+            cardProduto.setAttribute('id', elem.id);
+            
+            cardProduto.innerHTML = `
+            <img class="article__img--produto" src=${elem.imagem} alt="">
+            <h3 class="article__h3--tituloProduto">${elem.nome}</h3>
+            <div>
+                <p class="article__p--descricaoProduto">${elem.descricao}.</p>
+            </div>
+            <div class="article__div--categorias">
+                <span class="div__span--categoria">${elem.categoria}</span>
+            </div>
+            <div class="article__div--rodape">
+                <h3 class="div__h3--preco">R$ ${elem.preco.toFixed(2).replace('.',',')}</h3>
+                <button class="div__button--addCarrinho"> <img src="/src/imagens/botao-carrinho.png" alt="">
+                </button>
+            </div>
+            `;
+
+            divCard.appendChild(cardProduto);
+            
         }
         
     });
@@ -72,12 +73,15 @@ function filtroCategoria() {
     const categorias = document.getElementById('nav__ul--categorias');
     
     categorias.addEventListener('click', event => {
-        criarCards (event.target.id);
+
+        const input = event.target.id.toLowerCase();
+
+        criarCards (input);
     });
 
 }
 
-function abrirCadastroLogin(){
+function abrirCadastroLogin() {
 
     const botoes = document.getElementById('header__div--loginCadastro');
 
@@ -97,4 +101,16 @@ function abrirCadastroLogin(){
         
     });
     
+}
+
+function pesquisaDinamica() {
+
+    const input = document.getElementById('inputPesquisa');
+
+    input.addEventListener('keyup', () => {
+
+        const busca = input.value.toLowerCase();
+        
+        criarCards (busca);
+    })
 }
