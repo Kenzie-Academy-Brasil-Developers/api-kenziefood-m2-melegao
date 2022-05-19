51861cdd26b58919b92f0015347fcbdbe5864383
Débora Gomes Corrêa <99352568+DebCorrea@users.noreply.github.com>
2022-05-19 11:32:20 -0300

    fix: espaçamentos ajustados

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -1,12 +1,12 @@
-import {Produtos} from '../controller/Produtos.js'
+import {Produtos} from '../controller/Produtos.js';
 
 criarCards();
 
-async function criarCards(input){
+async function criarCards(input) {
 
     const produtos = await Produtos.listarProdutosEquipe();
     const divCard = document.getElementById('main__section--cards');
-    divCard.innerHTML = ``
+    divCard.innerHTML = ``;
     
     produtos.forEach((elem) => {
         
@@ -18,27 +18,28 @@ async function criarCards(input){
 
             cardProduto.innerHTML = `
                 <div class="div__div--cardInicio">
-                <img class="div__img--produto" src="${elem.imagem}" alt="${elem.nome}"/>
-                <div>
-                <h2 class="div__h2--titulo">${elem.nome}</h2>
+                    <img class="div__img--produto" src="${elem.imagem}" alt="${elem.nome}"/>
+                    <div>
+                        <h2 class="div__h2--titulo">${elem.nome}</h2>
+                    </div>
                 </div>
-            </div>
-            <div class="div__div--categorias">
-                <span class="div__span--categoria">${elem.categoria}</span>
-            </div>
-            <div>
-                <p class="div__p--descricao">${elem.descricao}</p>
-            </div>
-            <div class="div__div--botoes">
-                <button class="div__button--botaoLista">
-                <img src="/src/imagens/dashboard/botao-editar.png" alt="Editar" id="editar-${elem.id}"/>
-                </button>
-                <button class="div__button--botaoLista">
-                <img src="/src/imagens/dashboard/botao-excluir.png" alt="Excluir" id="excluir-${elem.id}"/>
-                </button>
-            </div>
-            `
-            divCard.appendChild(cardProduto)
+                <div class="div__div--categorias">
+                    <span class="div__span--categoria">${elem.categoria}</span>
+                </div>
+                <div>
+                    <p class="div__p--descricao">${elem.descricao}</p>
+                </div>
+                <div class="div__div--botoes">
+                    <button class="div__button--botaoLista">
+                        <img src="/src/imagens/dashboard/botao-editar.png" alt="Editar" id="editar-${elem.id}"/>
+                    </button>
+                    <button class="div__button--botaoLista">
+                        <img src="/src/imagens/dashboard/botao-excluir.png" alt="Excluir" id="excluir-${elem.id}"/>
+                    </button>
+                </div>
+                `;
+                divCard.appendChild(cardProduto)
+            
         } else if (elem.nome.toLowerCase().includes(input) || elem.categoria.toLowerCase().includes(input)) {
 
             const cardProduto = document.createElement('div');
@@ -47,26 +48,28 @@ async function criarCards(input){
 
             cardProduto.innerHTML = `
                 <div class="div__div--cardInicio">
-                <img class="div__img--produto" src="${elem.imagem}" alt="${elem.nome}"/>
-                <div>
-                <h2 class="div__h2--titulo">${elem.nome}</h2>
+                    <img class="div__img--produto" src="${elem.imagem}" alt="${elem.nome}"/>
+                    <div>
+                        <h2 class="div__h2--titulo">${elem.nome}</h2>
+                    </div>
                 </div>
-            </div>
-            <div class="div__div--categorias">
-                <span class="div__span--categoria">${elem.categoria}</span>
-            </div>
-            <div>
-                <p class="div__p--descricao">${elem.descricao}</p>
-            </div>
-            <div class="div__div--botoes">
-                <button class="div__button--botaoLista">
-                <img src="/src/imagens/dashboard/botao-editar.png" alt="Editar" id="editar-${elem.id}"/>
-                </button>
-                <button class="div__button--botaoLista">
-                <img src="/src/imagens/dashboard/botao-excluir.png" alt="Excluir" id="excluir-${elem.id}"/>
-                </button>
-            </div>
+                <div class="div__div--categorias">
+                    <span class="div__span--categoria">${elem.categoria}</span>
+                </div>
+                <div>
+                    <p class="div__p--descricao">${elem.descricao}</p>
+                </div>
+                <div class="div__div--botoes">
+                    <button class="div__button--botaoLista">
+                        <img src="/src/imagens/dashboard/botao-editar.png" alt="Editar" id="editar-${elem.id}"/>
+                    </button>
+                    <button class="div__button--botaoLista">
+                        <img src="/src/imagens/dashboard/botao-excluir.png" alt="Excluir" id="excluir-${elem.id}"/>
+                    </button>
+                </div>
             `
         };
+        
     });
+    
 }
